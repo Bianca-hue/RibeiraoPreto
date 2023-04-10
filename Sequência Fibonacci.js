e9817e636279c00ff8172759e712ba3a86e98d6f
Luana Siqueira Ronchi <luana.sronchi@gmail.com>
2023-04-10 11:03:42 -0300

    Add: Fibonacci

---
+++ Sequência Fibonacci.js
@@ -0,0 +1,23 @@
+function fibonnaci(numero){
+    let primeiro = 0, segundo = 1, proximo = 0;
+
+    while (proximo <= numero){
+        if (numero == proximo){
+            return true;
+            break;
+        }
+        primeiro = segundo;
+        segundo = proximo;
+        proximo = primeiro + segundo;
+    }
+    return false;
+}
+
+const numero = prompt("Insira o número que deseja verificar se existe na sequência de Fibonacci");
+const existe = fibonnaci(numero);
+
+if (existe){
+    console.log(`O número ${numero} pertence à sequência.`);
+}else{
+    console.log(`O número ${numero} não pertence à sequência.`);
+}
