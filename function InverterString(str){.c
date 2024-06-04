618cc529c5ddcb6e90898bc960e8359ae21c96ce
Bianca-hue <biancasilvasantos8@gmail.com>
2024-06-04 09:42:54 -0300

    Editado return( true|)

--- function InverterString(str){.c
+++ function InverterString(str){.c
@@ -1,3 +1,16 @@
+function InverterString(str){
+    let strInvertida = "";
+    
+    for(let i = str.length - 1; i >= 0; i--){
+        strInvertida += str[i];
+    }
+    console.log(strInvertida);
+}
+
+const str = prompt("Insira a string");
+InverterString(str);
+
+
 function fibonnaci(numero){
     let primeiro = 0, segundo = 1, proximo = 0;
 
